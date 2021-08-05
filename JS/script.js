dc324f716626028751d5e3499dab8bda8f39358d
Majedul Islam Leon <majedulleon@gmail.com>
2021-08-05 17:21:41 +0600

    test

---
+++ JS/script.js
@@ -0,0 +1,4 @@
+window.alert("JavaScript Learning");
+document.getElementById("p1").innerHTML = "Hello World";
+console.log("How's That ???");
+document.getElementById("h2").innerHTML="Now JavaScript is Connected";
