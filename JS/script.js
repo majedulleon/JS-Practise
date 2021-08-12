18ed46a2a28cbaae51dfc00eb666f10ee7fbb5ca
Majedul Islam Leon <majedulleon@gmail.com>
2021-08-12 13:24:35 +0600

    Square Addition program

--- JS/script.js
+++ JS/script.js
@@ -1,4 +1,19 @@
-window.alert("JavaScript Learning");
-document.getElementById("p1").innerHTML = "Hello World";
-console.log("How's That ???");
-document.getElementById("h2").innerHTML="Now JavaScript is Connected";
+//window.alert("JavaScript Learning");
+//document.getElementById("p1").innerHTML = "Hello World";
+//console.log("How's That ???");
+//document.getElementById("h2").innerHTML="Now JavaScript is Connected";
+
+var n = parseInt(prompt("Enter the number of terms: "));
+var sum = 0;
+var series = "";
+
+for (var i = 1; i <= n; i++) {
+    sum = sum + (i ** 2);
+    series = series + ((i ** 2).toString());
+    if (i == n) {
+        continue;
+    }
+    series = series + " + ";
+}
+
+console.log(`${series} = ${sum}`);
